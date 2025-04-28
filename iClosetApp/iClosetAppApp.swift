35d987cc1fd7a8d8b848d9939b0f031adaa71cce
Filip Tunega <tunega.f@gmail.com>
2025-04-28 10:09:29 +0200

    Initial Commit

---
+++ iClosetApp/iClosetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iClosetAppApp.swift
+//  iClosetApp
+//
+//  Created by Filip Tunega on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct iClosetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
